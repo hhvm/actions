fe9c57b0292633ce0d49d574f27a31595b446a43
Ján <jjergus@fb.com>
2021-01-19 15:20:01 -0800

    add skip_lint, skip_tests flags + test project (#3)

---
+++ hack-lint-test/lint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -e
+
+HHVM="$1"
+SKIP="$2"
+
+if [ "$SKIP" = "true" ]; then
+  echo "Lint skipped."
+  exit 0
+fi
+
+if [ "$SKIP" != "false" ]; then
+  echo "Invalid skip_lint value: $SKIP"
+  exit 1
+fi
+
+if [ "$HHVM" = "nightly" ]; then
+  echo "Lint skipped on nightly HHVM build."
+  exit 0
+fi
+
+echo "::group::Lint"
+vendor/bin/hhast-lint
+echo "::endgroup::"
