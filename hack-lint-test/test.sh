15b0f2726e2a868cc8d2ecf6eeb54587bdb5d8b6
jjergus <jjergus@fb.com>
2021-01-19 13:38:13 -0800

    add skip_lint, skip_tests flags

---
+++ hack-lint-test/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -e
+
+SKIP="$1"
+
+if [ "$SKIP" = "true" ]; then
+  echo "Tests skipped."
+  exit 0
+fi
+
+if [ "$SKIP" != "false" ]; then
+  echo "Invalid skip_tests value: $SKIP"
+  exit 1
+fi
+
+echo "::group::Run tests"
+vendor/bin/hacktest tests/
+echo "::endgroup::"
